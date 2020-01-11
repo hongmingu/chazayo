f08acab2e7370e75c8dd0ae3188441ac1964f058
hongmingu <gmh397@nate.com>
2020-01-11 19:38:31 +0900

    get_file_path sample

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -165,3 +166,25 @@ class ArmyInfo(models.Model):
 
     def __str__(self):
         return "UserUsername for %s" % self.user
+
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    # 파일의 확장자를 빼낸다.
+    usernum = instance.user.username
+    # 그 인스턴스의 유저고유 ID number 를 가져온다.
+
+    from django.utils.timezone import now
+    now = now()
+    # 현재시간을 가져온다.
+
+    now_date = now.strftime('%Y-%m-%d-%H-%M-%S')
+
+    # 현재시간을 스트링포맷으로 잡아준다.
+    import uuid
+    import os
+
+    filename = "300_%s_%s.%s" % (now_date, uuid.uuid4().hex, ext)
+    # uuid 는 랜덤 스트링 뽑아낼 때 씀
+
+    return os.path.join('photo/%s/userphoto' % usernum, filename)
